c592c406e0dc2a4b057fda34a9bd7a28cc5f5243
Emily Garrett <emilygarrett@Emilys-MacBook-Pro.local>
2018-02-15 21:30:41 -0800

    emily garrett

--- static/js/main.js
+++ static/js/main.js
@@ -5,8 +5,30 @@ console.log("JS good to go, sir!");
 // appropriate URL
 
 // code here ...
+$('.edit-game').submit(function(e) {
+	e.preventDefault();
+	var gameURL = $(this).attr('action');
+	var update = $(this).serialize();
+	$.ajax({
+		method: "PUT",
+		url: gameURL,
+		data: update
+	}).done(function() {
+		window.location = '/games';
+	});
+});
 
 // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
 // to the appropriate URL
 
 // code here ...
+
+$('.delete-game').on('click', function(e) {
+	var destination = $(this).attr('action');
+	$.ajax({
+		method: 'DELETE', 
+		url: destination
+	}).done(function() {
+		window.location = '/games';
+	});
+});
