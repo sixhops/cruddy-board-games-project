59ec6ce4ff44bbb7a93d108ca0e3d3d868191622
brantchyoga <brantrporter@gmail.com>
2018-02-15 17:01:56 -0800

    added get barebones

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var db = require('./models');
 
 var app = express();
 
@@ -19,10 +20,29 @@ app.get('/', function(req, res) {
 });
 
 // Add your routes here...
-
+app.get('/games', function(req, res) {
+  db.game.findAll().then(function(data) {
+    res.render('games/index', {games: data});
+  });
+});
 // GET /games - List of all games
+app.get('/games/new', function(req, res) {
+  res.render('games/new');
+});
 // GET /games/new - Returns a form for adding a new game
+app.post('/games', function(req, res) {
+  db.game.create({
+  name: req.body.name,
+    description: req.body.description
+  }).then(function(data) {
+    console.log(data);
+    res.redirect('/games');
+  });
+});
 // POST /games - Adds a new game from the posted form data
+app.get('/game/:name', function(req, res) {
+
+});
 // GET /games/:name - Gets one specific game
 // GET /games/:name/edit - Returns a form for editing a game's data
 // PUT /games/:name - Updates a game from the posted form data
