f2bdface3b8ad94f9fedef0f21a051a11d8c5841
Daniel Heyward <danheyward@gmail.com>
2018-02-16 07:43:37 -0800

    still a little buggy

--- static/js/main.js
+++ static/js/main.js
@@ -5,8 +5,28 @@ console.log("JS good to go, sir!");
 // appropriate URL
 
 // code here ...
-
+$('#edit').on('submit', function(e) {
+  e.preventDefault();
+  var url = $(this).attr('action');
+  var data = $(this).serialize();
+  $.ajax({
+    method: 'PUT',
+    url: url,
+    data: data
+  }).done(function(game) {
+    var newUrl = "/games/" + $('input[name="name"]').val();
+    window.location = newUrl;
+  });
+});
 // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
 // to the appropriate URL
-
-// code here ...
+$('.delete-').on("click", function(e) {
+   e.preventDefault();
+   var url = $(this).attr('href');
+   $.ajax({
+     method: 'DELETE',
+     url: gameUrl
+   }).done(function(data) {
+     window.location = "/games";
+   });
+ });
