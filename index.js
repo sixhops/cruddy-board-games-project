19742d53ead01ba703c3e1903b0a82e8750e52ae
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-02-15 20:10:59 -0800

    inital commit - partials in, post operational, i think all pages are there

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var db = require('./models');
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -14,18 +15,54 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
 // Default route for the root of the site
-app.get('/', function(req, res) {
+app.get('/', function(req,res) {
   res.render('home');
 });
 
 // Add your routes here...
-
 // GET /games - List of all games
+app.get('/games', function(req,res) {
+  db.game.findAll().then(function(data){
+  // console.log(data);
+  // console.log(data[0].datavValues.name);
+  // console.log(data[0].datavValues.description);
+
+  // games will be an array of all game instances
+  res.render('games/index', {games: data});
+  });
+});
+
 // GET /games/new - Returns a form for adding a new game
+app.get('/games/new', function(req,res){
+  res.render('games/new');
+});
+
 // POST /games - Adds a new game from the posted form data
+app.post('/games', function(req,res){
+  db.game.create({
+    name: req.body.name,
+    description: req.body.description
+    }).then(function(data) {
+        // you can now access the newly created task via the variable data
+        console.log(data);
+        res.render('games/name', {game: data});
+    });
+});
+
 // GET /games/:name - Gets one specific game
+app.get('/games/:name', function(req,res){
+  console.log("in games/:name path.....!");
+  console.log(req.body.name);
+
+});
 // GET /games/:name/edit - Returns a form for editing a game's data
+app.get('/games/:name/edit', function(req,res){
+  console.log("in games/:name/edit path.....!");
+  console.log(data.body.name);
+
+});
 // PUT /games/:name - Updates a game from the posted form data
+
 // DELETE /games/:name - Delete one specific game
 
 // start the server
