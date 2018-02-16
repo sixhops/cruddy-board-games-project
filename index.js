67f6ef46e32b4a60bc34aa9b95776ab9568b4ce6
Brett <brettspencr@gmail.com>
2018-02-15 19:31:09 -0800

    Finished deliverable, but no styling yet

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var db = require('./models');
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -21,12 +22,70 @@ app.get('/', function(req, res) {
 // Add your routes here...
 
 // GET /games - List of all games
+app.get('/games', function(req, res){
+    var games = db.game.findAll().then(function(data){
+        res.render('games/index', { games : data });
+    });
+});
+
 // GET /games/new - Returns a form for adding a new game
+app.get('/games/new', function(req, res){
+    res.render('games/new');
+});
+
 // POST /games - Adds a new game from the posted form data
+app.post('/games', function(req, res){
+    db.game.create({
+        name: req.body.name,
+        description: req.body.description
+    }).then(function(data){
+        res.redirect('/games');
+    });
+});
+
 // GET /games/:name - Gets one specific game
+app.get('/games/:name', function(req, res){
+    db.game.find({
+        where: { name: req.params.name }
+    }).then(function(game){
+        res.render('games/game', { game : game.dataValues});
+    });
+});
+
+
 // GET /games/:name/edit - Returns a form for editing a game's data
+app.get('/games/:name/edit', function(req, res){
+    db.game.find({
+        where: { name: req.params.name }
+    }).then(function(game){
+        res.render('games/edit', { game : game.dataValues});
+    });
+});
+
 // PUT /games/:name - Updates a game from the posted form data
+app.put('/games/:name', function(req, res){
+    db.game.update({
+        name: req.body.name,
+        description : req.body.description
+    }, {
+        where: { name: req.params.name } 
+    }).then(function(data){
+        console.log(data);
+        res.send('Success. Go ahead and redirect, jQuery.');
+    })
+});
+
 // DELETE /games/:name - Delete one specific game
+app.delete('/games/:name', function(req, res){
+    db.game.destroy({
+        where: { name: req.params.name }
+    }).then(function(){
+        console.log(req.params.name + " was deleted.");
+        res.send('Success');
+    })
+});
+
+
 
 // start the server
 var port = 3000;
