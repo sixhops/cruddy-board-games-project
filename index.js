c7ebeaf27b0e662cc9800f53811adc273bcb5709
hannan-emily <emilyhannan@gmail.com>
2018-02-15 19:30:53 -0800

    all but delete works now

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var db = require('./models');
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -21,12 +22,57 @@ app.get('/', function(req, res) {
 // Add your routes here...
 
 // GET /games - List of all games
+app.get('/games', function(req, res) {
+  db.game.findAll().then(function(data) {
+    console.log(data);
+    res.render('games/index', {games: data});
+  });
+});
+
 // GET /games/new - Returns a form for adding a new game
+app.get('/games/new', function(req, res) {
+  res.render('games/new');
+});
+
 // POST /games - Adds a new game from the posted form data
+app.post('/games', function(req, res) {
+  db.game.create({
+  name: req.body.name,
+  description: req.body.description
+}).then(function(data) {
+  console.log(data);
+  res.redirect('/games');
+});
+});
+
 // GET /games/:name - Gets one specific game
+app.get('/games/:name', function(req, res) { //READ function
+  db.game.find({
+    where: {name: req.params.name}
+  }).then(function(data) {
+    res.render('games/show', {game: data});
+  })
+});
+
 // GET /games/:name/edit - Returns a form for editing a game's data
+app.get('/games/:name/edit', function(req, res) {
+  db.game.findAll().then(function(data) {
+    console.log(data);
+    res.render('games/edit', {game: data});
+  });
+});
+
 // PUT /games/:name - Updates a game from the posted form data
-// DELETE /games/:name - Delete one specific game
+app.put('/games/:name/', function(req, res) {
+  db.game.find({
+    where: {name: req.params.name}
+  }).then(function(data) {
+    res.send("");
+  })
+});
+
+
+// DELETE /games/:name - Delee one specific game
 
 // start the server
 var port = 3000;
--- static/js/main.js
+++ static/js/main.js
@@ -10,3 +10,22 @@ console.log("JS good to go, sir!");
 // to the appropriate URL
 
 // code here ...
+
+
+$('#edit-form').on('submit', function(e) {
+  e.preventDefault();
+  var gameElement = $(this);
+  var gameUrl = gameElement.attr('action');
+  var gameData = gameElement.serialize();
+  $.ajax({
+    method: 'PUT',
+    url: gameUrl,
+    data: gameData
+  }).done(function(data) {
+    // get data returned from the PUT route
+    console.log(data);
+
+    // // or, you can redirect to another page
+    window.location = '/games';
+  });
+});
