4012ddad1fe6f75036670c15bdd09f1020705a85
Scott <scrosehart@gmail.com>
2018-02-16 09:01:57 -0800

    Scott Rosehart - all changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,31 @@
-console.log("JS good to go, sir!");
 
 // listen for a "submit" event for the edit page. the edit form will live
 // at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
 // appropriate URL
 
-// code here ...
+$('#edit-submit').on('submit', function(e) {
+  e.preventDefault();
+  var gameUrl = $(this).attr('action');
+  var gameData = $(this).serialize();
+  $.ajax({
+    method: 'put',
+    url: gameUrl,
+    data: gameData
+  }).done(function() {
+    window.location = '/games';
+  });
+});
 
 // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
 // to the appropriate URL
 
-// code here ...
+$('.delete').on('click', function(e) {
+  e.preventDefault();
+  var deleteUrl = $(this).attr('href');
+  $.ajax({
+    method: 'delete',
+    url: deleteUrl
+  }).done(function(data) {
+    window.location = '/games';
+  });
+});
