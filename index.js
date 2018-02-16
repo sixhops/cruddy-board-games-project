7d8f24d5f63db00fc3eb66920f5a97def6d3065d
Kyle Van Bergen <kyleavb@gmail.com>
2018-02-15 19:50:11 -0800

    completed all deliverables

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var db = require("./models");
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+
 
 var app = express();
 
@@ -21,14 +23,60 @@ app.get('/', function(req, res) {
 // Add your routes here...
 
 // GET /games - List of all games
+app.get("/games", function(req, res){
+  db.game.findAll().then(function(data){
+    res.render("games/index", {games: data})
+  });
+});
 // GET /games/new - Returns a form for adding a new game
+app.get("/games/new", function(req, res){
+  res.render("games/new");
+});
 // POST /games - Adds a new game from the posted form data
+app.post("/games", function(req, res){
+  db.game.create({
+    name: req.body.name,
+    description: req.body.description
+  }).then(function(data){
+    res.redirect("/games")
+  });
+});
 // GET /games/:name - Gets one specific game
+app.get("/games/:name", function(req, res){
+  db.game.findAll({
+    where: {name: req.params.name}
+  }).then(function(data){
+    res.render("games/show", {game: data});
+  });
+});
 // GET /games/:name/edit - Returns a form for editing a game's data
+app.get("/games/:name/edit", function(req, res){
+  db.game.findAll({
+    where: {name: req.params.name}
+  }).then(function(data){
+    res.render("games/edit", {game: data});
+  });
+});
 // PUT /games/:name - Updates a game from the posted form data
+app.put("/games/:name", function(req, res){
+  db.game.update({
+    name: req.body.name,
+    description: req.body.description
+  }, {
+    where: {name: req.params.name}
+  }).then(function(){
+    res.send();
+  });
+});
 // DELETE /games/:name - Delete one specific game
-
+app.delete("/games/:name",function(req, res){
+  db.game.destroy({
+    where: {name: req.params.name}
+  }).then(function(){
+    res.send();
+  });
+});
 // start the server
-var port = 3000;
+var port = 5000;
 console.log("Listening at http://localhost:" + port + "...");
 app.listen(port);
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,29 @@
-console.log("JS good to go, sir!");
-
 // listen for a "submit" event for the edit page. the edit form will live
 // at the path GET /games/:name/edit. Use AJAX to send a PUT request to the
 // appropriate URL
 
-// code here ...
+$(".edit-game").submit(function(e){
+  e.preventDefault();
+  var gameUrl = $(this).attr('action');
+  var updatedData = $(this).serialize();
+  $.ajax({
+    method: "PUT",
+    url: gameUrl,
+    data: updatedData,
+  }).done(function(){
+    window.location = "/games";
+  });
+})
 
 // listen for clicks on "delete" links. use AJAX to send a DELETE HTTP request
 // to the appropriate URL
 
-// code here ...
+$(".delete-game").on('click', function(e){
+  var destUrl = $(this).attr('action');
+  $.ajax({
+    method: "DELETE",
+    url: destUrl,
+  }).done(function(){
+    window.location = "/games";
+  });
+});
