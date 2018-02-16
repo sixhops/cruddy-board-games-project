ef40708e7dc3de901d940fa489960fe9053b8b6a
Michael Hammer <michaelhammer@Michaels-MacBook-Pro.local>
2018-02-16 10:07:36 -0800

    Michael H cruddy-b

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var db = require('./models');
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -21,13 +22,69 @@ app.get('/', function(req, res) {
 // Add your routes here...
 
 // GET /games - List of all games
+app.get('/games', function(req, res) {
+  db.game.findAll().then(function(data) {
+  // console.log(data);
+  // console.log(data.[0].dataValues.name);
+  // console.log(data.[0].dataValues.description);
+  res.render('games/index', {games: data});
+  // data will be an array of all User instances
+  });
+});
 // GET /games/new - Returns a form for adding a new game
+app.get('/games/new', function(req, res) {
+  res.render('games/new');
+});
 // POST /games - Adds a new game from the posted form data
+app.post('/games', function(req, res) {
+  db.game.create({
+    name: req.body.name,
+    description: req.body.description
+  }).then(function(data) {
+    // you can now access the newly created task via the variable data
+    //console.log(data);
+    res.redirect('/games');
+  });
+});
 // GET /games/:name - Gets one specific game
+app.get('/games/:name', function(req, res) {
+  db.game.find({
+    where: {name: req.params.name}
+  }).then(function(data) {
+    res.render('games/show', {game: data});
+  })
+});
 // GET /games/:name/edit - Returns a form for editing a game's data
+app.get('/games/:name/edit', function(req, res) {
+  db.game.find({
+    where: {name: req.params.name}
+  }).then(function(data) {
+    res.render('games/edit', {game: data});
+  })
+});
 // PUT /games/:name - Updates a game from the posted form data
+app.put('/games/:name', function(req, res) {
+  db.game.update({
+    name: req.body.name,
+    description: req.body.description
+  }, {
+    where: {
+      name: req.params.name
+    }
+  }).then(function(data) {
+    // do something when done updating
+    res.send('success');
+  })
+});
 // DELETE /games/:name - Delete one specific game
-
+app.delete('/games/:name', function(req, res) {
+  db.game.destroy({
+    where: { name: req.params.name }
+  }).then(function(data) {
+    // do something when done deleting
+    res.send('success');
+  })
+});
 // start the server
 var port = 3000;
 console.log("Listening at http://localhost:" + port + "...");
